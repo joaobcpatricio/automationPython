7920e967059afd45629f0bd196d0f6cff7aaf03d
joaobcpatricio <castanheirapatricio@outlook.com>
2020-10-22 21:05:40 +0100

    Create a new repository

--- create.py
+++ create.py
@@ -16,6 +16,11 @@ def create():
     python_button.send_keys('Pomares18')
     python_button = browser.find_elements_by_xpath("/html/body/div[3]/main/div/form/div[4]/input[12]")[0]   
     python_button.click()
+    browser.get('http://github.com/new')
+    python_button = browser.find_elements_by_xpath("//*[@id='repository_name']")[0]    
+    python_button.send_keys('AutoProject')
+    python_button = browser.find_element_by_css_selector('button.first-in-line')   
+    python_button.submit()
 
 #print("Hello");
 
